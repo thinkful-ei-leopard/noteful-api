19343e6ba08432024bf2a8f04bfc27689b201c46
Jose <jozylopez27@gmail.com>
2020-03-02 16:26:42 -0600

    json server reconfig

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
+const knex = require('knex')
 const app = require('./app')
-const { PORT } = require('./config')
+const { PORT, DB_URL } = require('./config')
 
+const knexInstance = knex({
+    client: 'pg',
+    connection: DB_URL,
+})
+
+app.set('knexInstance', knexInstance)
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
